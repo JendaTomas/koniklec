c4b9050297e5e4c4837df40709f681d795eb5f77
JendaTomas <jenda.tomas@seznam.cz>
2023-09-26 10:59:30 +0200

    pridani popisku

---
+++ zkouska.py
@@ -0,0 +1,30 @@
+lektori= ["Jenda","Jirka","Bohouš","Janča","Leona"]
+evp= ["Bezouši","HH","Stavitelé města"]
+cislo_evp= int(input("číslo EVP:"))
+ 
+odpoved_umi= input("umíš"+evp[cislo_evp]+"?")
+
+if odpoved_umi == "ano" or odpoved_umi=="Ano":
+    odpoved_umi == True 
+elif odpoved_umi == "ne" or odpoved_umi=="Ne":
+    odpoved_umi == False
+else:
+    print ("napiš prosím \"ano\" či \"ne\": ")
+
+
+
+
+#umi= lektori[] 
+
+# ma_cas= 
+
+
+
+#def kdo_muze(evp,kdy):
+
+
+
+
+
+
+
